3be53792269b4788bbb4890aed45c9f7245f409d
Deniska26 <68137655+Deniska26@users.noreply.github.com>
2020-10-15 18:19:47 +0700

    new

---
+++ ProjectCalc/ProjectCalc/tests/first_tests.py
@@ -0,0 +1,33 @@
+
+
+import pytest
+from app.calculator import Calculator
+
+class Testcalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculator_correctly(self):
+        assert self.calc.multiply(self, 10, 5) == 50
+
+    def test_multyply_calculator_not_correctly(self):
+        assert self.calc.multiply(self,2, 2) == 5
+
+    def test_division_calculator_correctly(self):
+        assert self.calc.division(self, 10, 5) == 2
+
+    def test_division_calculator_not_correctly(self):
+        assert self.calc.division(self, 10, 5) == 3
+
+    def test_subtraction_calculator_correctly(self):
+        assert self.calc.subtraction(self, 10, 5) == 5
+
+    def test_subtraction_calculator_not_correctly(self):
+        assert self.calc.subtraction(self, 10, 5) == 1
+
+    def test_adding_calculator_correctly(self):
+        assert self.calc.adding(self, 10, 5) == 15
+
+    def test_adding_calculator_not_correctly(self):
+        assert self.calc.adding(self, 10, 5) == 10
+
